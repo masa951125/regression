867484ed72af6e881cd876fbac3352230ab0d8d8
masa951125 <masayoshi418979@icloud.com>
2020-12-29 16:49:24 +0900

    file changed

--- regression_preparation.R
+++ regression_preparation.R
@@ -1,6 +1,19 @@
 library(tidyverse)
 library(HistData)
 install.packages("HistData")
-data("Galton")
-install.packages("Galton")
-library(dslabs)
+data("GaltonFamilies")
+head(GaltonFamilies)
+
+galton_heights <-GaltonFamilies %>%
+filter(gender == "male") %>%
+  group_by(family) %>%
+  sample_n(1) %>%
+  ungroup() %>%
+  select(father, childHeight) %>%
+  rename(son = childHeight)
+
+galton_heights %>%
+  summarize(mean(father),sd(father), mean(son), sd(son))
+
+galton_heights %>%
+  ggplot(aes(father,son))+geom_point(alpha=0.5)
