cabd19020c29f0ce1a56f6f0dcd73c32f4ecc5fa
masayoshi sato <masayoshi418979@icloud.com>
2021-02-02 23:57:57 +0900

    changed

--- Introduction_to_regression.R
+++ Introduction_to_regression.R
@@ -47,5 +47,62 @@ Teams %>% filter(yearID %in% 1961:2001) %>%
   ggplot(aes(X2B_per_game, X3B_per_game)) + 
   geom_point(alpha = 0.5)
 
-  
-  
+#Galton_data
+
+install.packages("HistData")
+library(HistData)
+GaltonFamilies
+?HistData
+str(GaltonFamilies)
+
+galton_heights <- GaltonFamilies %>%
+  filter(gender == "male") %>%
+  group_by(family) %>%
+  sample_n(1) %>%
+  ungroup() %>%
+  select(father, childHeight) %>%
+  rename(son = childHeight)
+data("GaltonFamilies")
+set.seed(1983)
+
+galton_heights %>% summarize(mean(father), sd(father), mean(son), sd(son))
+galton_heights %>%
+  ggplot(aes(father, son)) +
+  geom_point(alpha = 0.5)  
+
+galton_heights %>% summarize(r = cor(father, son)) %>% pull(r)
+
+R <- sample_n(galton_heights, 25, replace = TRUE) %>%
+  summarize(r = cor(father, son))
+R
+
+B <- 1000
+n <- 50
+
+R <- replicate(B,{
+  sample_n(galton_heights, n, replace = T) %>%
+  summarize(r = cor(father,son))  %>%
+  pull(r)
+  })
+qplot(R, geom = "histogram", binwidth = 0.05, color = I("black"))
+
+mean(R)
+sd(R)
+
+data.frame(R) %>%
+  ggplot(aes(sample = R)) +
+  stat_qq() +
+  geom_abline(intercept = mean(R), slope = sqrt((1-mean(R)^2)/(n-2)))
+
+str(Teams)
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  summarize(r = cor( AB / G, R / G)) %>%
+  pull(r)
+
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  summarize(r = cor( W / G, E / G)) %>%
+  pull(r)
+
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  summarize(r = cor( X2B / G, X3B / G)) %>%
+  pull(r)
