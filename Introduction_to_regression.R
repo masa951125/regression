07f0748570c0c6aed6f1aa563a0b3c7d3ad276a6
masayoshi sato <masayoshi418979@icloud.com>
2021-02-10 00:08:48 +0900

    changed a bit

--- Introduction_to_regression.R
+++ Introduction_to_regression.R
@@ -106,3 +106,64 @@ Teams %>% filter(yearID %in% 1961:2001) %>%
 Teams %>% filter(yearID %in% 1961:2001) %>%
   summarize(r = cor( X2B / G, X3B / G)) %>%
   pull(r)
+
+#Stratification
+
+sum(galton_heights$father == 72)
+sum(galton_heights$father == 72.5)
+
+conditional_avg <- galton_heights %>%
+  filter(round(father) == 72) %>%
+  summarize(avg = mean(son)) %>%
+  pull(avg)
+conditional_avg
+
+round(galton_heights$father)
+?factor
+
+# stratify fathers' heights to make a boxplot of son heights
+galton_heights %>% mutate(father_strata =factor(round(father))) %>%
+  ggplot(aes(father_strata, son)) +
+  geom_boxplot() +
+  geom_point()
+
+# center of each boxplot
+galton_heights %>%
+  mutate(father = round(father)) %>%
+  group_by(father) %>%
+  summarize(son_conditional_avg = mean(son)) %>%
+  ggplot(aes(father, son_conditional_avg)) +
+  geom_point()  
+
+# calculate values to plot regression line on original data
+mu_x <- mean(galton_heights$father)
+mu_y <- mean(galton_heights$son)
+s_x <- sd(galton_heights$father)
+s_y <- sd(galton_heights$son)
+r <- cor(galton_heights$father, galton_heights$son)
+m <- r * s_y/s_x
+b <- mu_y - m*mu_x
+
+galton_heights %>%
+  ggplot(aes(father, son)) +
+  geom_point(alpha = 0.5) +
+  geom_abline(intercept = b, slope = m)
+
+galton_heights %>%
+  mutate(z_father = round((father - mean(father)) / sd(father))) %>%
+  filter(z_father %in% -2:2) %>%
+  ggplot() +  
+  stat_qq(aes(sample = son)) +
+  facet_wrap( ~ z_father) 
+
+m_1 <-  r * s_y / s_x
+b_1 <- mu_y - m_1*mu_x
+
+m_2 <-  r * s_x / s_y
+b_2 <- mu_x - m_2*mu_y
+
+galton_heights %>%
+  ggplot(aes(son, father)) +
+  geom_point(alpha = 0.5) +
+  geom_abline(intercept = b_2, slope = m_2)
+
