77d3308a492582ba717d692bb57774dd727188be
masayoshi sato <masayoshi418979@icloud.com>
2021-02-14 21:19:52 +0900

    New file

---
+++ Linear_models.R
@@ -0,0 +1,89 @@
+library(Lahman)
+library(tidyverse)
+library(dslabs)
+Teams
+
+bb_slope <- Teams %>% 
+  filter(yearID %in% 1961:2001 ) %>% 
+  mutate(BB_per_game = BB/G, R_per_game = R/G) %>% 
+  lm(R_per_game ~ BB_per_game, data = .) %>% 
+  .$coef %>%
+  .[2]
+bb_slope
+
+bb_slope <- Teams %>% 
+  filter(yearID %in% 1961:2001 ) %>% 
+  mutate(BB_per_game = BB/G, R_per_game = R/G) 
+
+r <- cor(bb_slope$R_per_game,bb_slope$BB_per_game)
+sd_b <- sd(bb_slope$BB_per_game)
+sd_r <- sd(bb_slope$R_per_game)
+m <- r*sd_r/sd_b
+m
+
+get_slope <- function(x, y) cor(x, y) * sd(y) / sd(x)
+get_slope(bb_slope$BB_per_game,bb_slope$R_per_game)
+
+summary_stats <- Teams %>% 
+     filter(yearID %in% 1961:2001 ) %>%
+     mutate(HR_per_game = HR/G, R_per_game = R/G) %>%
+     summarize(avg_HR = mean(HR_per_game),
+                            s_HR = sd(HR_per_game),
+                            avg_R = mean(R_per_game),
+                            s_R = sd(R_per_game),
+                            r = cor(HR_per_game, R_per_game))
+summary_stats
+
+bb_slope <- Teams %>% 
+  filter(yearID %in% 1961:2001 ) %>% 
+  mutate(BB_per_game = BB/G, R_per_game = R/G) %>% 
+  summarize(slope = get_slope(BB_per_game, R_per_game))
+
+bb_slope 
+
+singles_slope <- Teams %>% 
+  filter(yearID %in% 1961:2001 ) %>%
+  mutate(Singles_per_game = (H-HR-X2B-X3B)/G, R_per_game = R/G) %>%
+  summarize(slope = get_slope(Singles_per_game, R_per_game))
+
+singles_slope 
+
+Teams %>% 
+  filter(yearID %in% 1961:2001 ) %>% 
+  mutate(Singles = (H-HR-X2B-X3B)/G, BB = BB/G, HR = HR/G) %>%  
+  summarize(cor(BB, HR), cor(Singles, HR), cor(BB,Singles))
+
+# stratify HR per game to nearest 10, filter out strata with few points
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR_strata = round(HR/G, 1), 
+         BB_per_game = BB / G,
+         R_per_game = R / G) %>%
+  filter(HR_strata >= 0.4 & HR_strata <=1.2) 
+
+# scatterplot for each HR stratum
+dat %>% 
+  ggplot(aes(BB_per_game, R_per_game)) +  
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm") +
+  facet_wrap( ~ HR_strata)
+
+dat %>%  
+  group_by(HR_strata) %>%
+  summarize(slope = get_slope(BB_per_game, R_per_game))
+
+# stratify by BB
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(BB_strata = round(BB/G, 1), 
+         HR_per_game = HR / G,
+         R_per_game = R / G) %>%
+  filter(BB_strata >= 2.8 & BB_strata <=3.9) 
+
+# scatterplot for each BB stratum
+dat %>% ggplot(aes(HR_per_game, R_per_game)) +  
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm") +
+  facet_wrap( ~ BB_strata)
+
+dat %>%  
+  group_by(BB_strata) %>%
+  summarize(slope = cor(HR_per_game, R_per_game)*sd(R_per_game)/sd(HR_per_game)) 
