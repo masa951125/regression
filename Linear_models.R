f95e9a0af93ffe831233b8b38ed3129abf66b9c0
masayoshi sato <masayoshi418979@icloud.com>
2021-02-15 23:21:41 +0900

    file changed

--- Linear_models.R
+++ Linear_models.R
@@ -117,3 +117,88 @@ fit <- lm(son ~ father, data = galton_heights)
 fit
 
 summary(fit)
+
+# Monte Carlo simulation
+B <- 1000
+N <- 50
+lse <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>% 
+    lm(son ~ father, data=.) %>% 
+    .$coef 
+})
+
+lse <- data.frame(beta_0 = lse[1,], beta_1 = lse[2,]) 
+
+install.packages("gridExtra")
+library(gridExtra)
+
+#both are normal distribution
+p1 <- lse %>% ggplot(aes(beta_0)) + geom_histogram(binwidth = 5, color = "black")  
+p2 <- lse %>% ggplot(aes(beta_1)) + geom_histogram(binwidth = 0.1, color = "black") 
+
+grid.arrange(p1, p2, ncol = 2)
+
+# summary statistics
+sample_n(galton_heights, N, replace = TRUE) %>% 
+  lm(son ~ father, data = .) %>% 
+  summary %>%
+  .$coef
+
+lse %>% summarize(se_0 = sd(beta_0), se_1 = sd(beta_1))
+
+lse %>% summarize(cor(beta_0, beta_1))
+
+B <- 1000
+N <- 50
+lse <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>%
+    mutate(father = father - mean(father)) %>%
+    lm(son ~ father, data = .) %>% .$coef 
+})
+
+cor(lse[1,], lse[2,]) 
+
+#predictions and confidence intervals
+
+galton_heights %>% ggplot(aes(son, father))+
+  geom_point()+ geom_smooth(method = "lm")
+
+# predict Y directly
+fit <- galton_heights %>% lm(son ~ father, data = .) 
+Y_hat <- predict(fit, se.fit = TRUE)
+names(Y_hat)
+
+galton_heights %>%
+  mutate(Y_hat = predict(lm(son ~ father, data=.))) %>%
+  ggplot(aes(father, Y_hat))+
+  geom_line()
+mean(fit)
+Y_hat
+
+Y_hat %>% pull(fit)
+
+#assessment
+
+beta1 = seq(0, 1, len=nrow(galton_heights))
+results <- data.frame(beta1 = beta1,
+                      rss = sapply(beta1, rss, beta0 = 36))
+results %>% ggplot(aes(beta1, rss)) + geom_line() + 
+  geom_line(aes(beta1, rss), col=2)
+
+Teams %>% 
+  filter(yearID %in% 1961:2001 )%>%
+  mutate(R_per_game = R/G, BB_per_game = BB/G, HR_per_game =HR/G) %>%
+  lm(R_per_game ~ BB_per_game+HR_per_game , data= .)
+
+model <- lm(son ~ father, data = galton_heights)
+predictions <- predict(model, interval = c("confidence"), level = 0.95)
+data <- as.tibble(predictions) %>% bind_cols(father = galton_heights$father)
+
+ggplot(data, aes(x = father, y = fit)) +
+  geom_line(color = "blue", size = 1) + 
+  geom_ribbon(aes(ymin=lwr, ymax=upr), alpha=0.2) + 
+  geom_point(data = galton_heights, aes(x = father, y = son))
+
+
+
+
