82d7f684af15e117b0f12d333e5a79afb6c4ed2a
masayoshi sato <masayoshi418979@icloud.com>
2021-02-21 10:56:22 +0900

    file changed

--- Linear_models.R
+++ Linear_models.R
@@ -1,7 +1,8 @@
 library(Lahman)
 library(tidyverse)
 library(dslabs)
-Teams
+library(HistData)
+data("GaltonFamilies")
 
 bb_slope <- Teams %>% 
   filter(yearID %in% 1961:2001 ) %>% 
@@ -262,3 +263,87 @@ summary(singles)
 
 bb <- lm(bb~ mean_bb,data=combined_data)
 summary(bb)
+
+#tibbles
+
+# stratify by HR
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR = round(HR/G, 1), 
+         BB = BB/G,
+         R = R/G) %>%
+  select(HR, BB, R) %>%
+  filter(HR >= 0.4 & HR<=1.2)
+
+# calculate slope of regression lines to predict runs by BB in different HR strata
+dat %>%  
+  group_by(HR) %>%
+  summarize(slope = cor(BB,R)*sd(R)/sd(BB))
+
+dat %>%  
+  group_by(HR) %>%
+  lm(R ~ BB, data = .) %>%
+  .$coef
+
+dat %>% group_by(HR) %>% head()
+dat %>% group_by(HR) %>% class()
+
+tibble(id = c(1, 2, 3), func = c(mean, median, sd))
+
+# data frames with multiple rows will be concatenated appropriately
+get_lse <- function(data){
+  fit <- lm(R ~ BB, data = data)
+  data.frame(term = names(fit$coefficients),
+             estimate = fit$coefficients, 
+             se = summary(fit)$coefficient[,2])
+}
+
+get_slope <- function(data){
+  fit <- lm(R ~ BB, data = data)
+  data.frame(slope = fit$coefficients[2], 
+             se = summary(fit)$coefficient[2,2])
+}
+
+dat %>%  
+  group_by(HR) %>%
+  do(get_slope(.))
+
+dat %>%  
+  group_by(HR) %>%
+  do(slope = get_slope(.))
+
+get_lse <- function(data){
+  fit <- lm(R ~ BB, data = data)
+  data.frame(term = names(fit$coefficients),
+             estimate = fit$coefficients, 
+             se = summary(fit)$coefficient[,2])
+}
+
+dat %>%  
+  group_by(HR) %>%
+  do(get_lse(.))
+
+#broom
+library(broom)
+fit <- lm(R ~ BB, data = dat)
+tidy(fit)
+
+tidy(fit, conf.int = T)
+
+# pipeline with lm, do, tidy
+dat %>%  
+  group_by(HR) %>%
+  do(tidy(lm(R ~ BB, data = .), conf.int = TRUE)) %>%
+  filter(term == "BB") %>%
+  select(HR, estimate, conf.low, conf.high)
+
+dat %>%  
+  group_by(HR) %>%
+  do(tidy(lm(R ~ BB, data = .), conf.int = TRUE)) %>%
+  filter(term == "BB") %>%
+  select(HR, estimate, conf.low, conf.high) %>%
+  ggplot(aes(HR, y = estimate, ymin = conf.low, ymax = conf.high)) +
+  geom_errorbar() +
+  geom_point()
+
+glance(fit)
+augment(fit)
